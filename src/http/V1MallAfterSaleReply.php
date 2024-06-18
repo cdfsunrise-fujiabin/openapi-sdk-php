d7a239107689c165491d5a9823144818cf487954
fujiabin <fujiabin@cdfsunrise.com>
2024-06-18 11:03:26 +0800

    增加/v1/mall/delivery/change、/v1/mall/express、/v1/mall/invoice/apply、/v1/mall/order/detail

---
+++ src/http/V1MallAfterSaleReply.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace OpenApi;
+
+require_once(__DIR__.'/../../index.php');
+require_once(__DIR__.'/../utils/RsaHelper.php');
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+use utils\RsaHelper;
+
+class V1MallAfterSaleReply
+{
+    /**
+        V1MallAfterSaleReply
+         *Description: 【商户入驻】- 售后信息回执
+         * @param: body BaseRequest BaseRequest 必填项
+         * @throws GuzzleException
+     */
+    public function call($host, $authToken, $body)
+    {
+        $client = new Client(['headers' => ['Authorization' => $authToken]]);
+        
+        $response = $client->request('Post', $host . sprintf("/v1/mall/after/sale/reply"), [
+            'json' => $body
+        ]);
+        
+        return json_decode($response->getBody()->getContents());
+    }
+}
