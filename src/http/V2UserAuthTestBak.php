e2762010a99aa1618b7f55a9eae94c44349aed74
fujiabin <fujiabin@cdfsunrise.com>
2024-07-12 10:31:48 +0800

    更新版本

--- src/http/V2UserAuthTestBak.php
+++ src/http/V2UserAuthTestBak.php
@@ -17,7 +17,8 @@ class V2UserAuthTestBak extends TestCase {
 			"password" => RsaHelper::rsaEncrypt("123456", "-----BEGIN RSA PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA0XAbluGzEsRhZ82X20bI\nvUPxtTLiBJQiDfJtpLF4YjmxB04cU9LHAIGsxnK/VDcnlIlOutneJarxWwVq5sEe\nVp9bMG+KtNPPdXFj+tMb4hloKwFAVXiyxLoDrgRW5DDes8MUI+6kiIGX4hi5KSRP\nmKFVtMOLD142+kuRaCEYvz4gz85cRiOa09jJ8JEvU+8DieysJJrEvVVaexGjJD3o\nhFslRLfoG06NbvaSwdqL1+z98pdp4JMjx47BccUTEXB1jVVOmU3zyNVP33v4iVyW\nq5O47ccKVMuvxN5dlGsXEOoSuesWxQblF2TjNt1Vd8D73l7por4Gm7Gf4Mw05Tyl\nJwIDAQAB\n-----END RSA PUBLIC KEY-----"),
         ];
         $ret = $t->call("http://lefox-marketing-openapi-gateway-dev.cdfsunrise.com", $rawParams);
-        print_r($ret);
+        $rawData = RsaHelper::rsaDecrypt($ret->data, "");
+        print_r($rawData);
         $this->assertNotEquals("", $ret->requestId);
     }
 }
